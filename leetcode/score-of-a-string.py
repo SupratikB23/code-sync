2927c04d5561973352d0a74a30be55f69a0f38d6
Supratik Bhowal <supratikbhowal23@gmail.com>
2025-09-18 22:37:10 +0530

    Add solution for score-of-a-string

---
+++ leetcode/score-of-a-string.py
@@ -0,0 +1,7 @@
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        num = 0
+        for i in range(len(s)-1):
+            a = ord(s[i]) - ord(s[i+1])
+            num += abs(a)
+        return num
