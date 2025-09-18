d63d61a837abcc347ee8472d223d6625c8f61bd8
Supratik Bhowal <supratikbhowal23@gmail.com>
2025-09-19 00:09:04 +0530

    Add solution for group-anagrams

---
+++ leetcode/group-anagrams.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d = {}
+        for i in strs:
+            s = "".join(sorted(i))
+            if s in d:
+                d[s].append(i)
+            else:
+                d[s] = [i]
+        return d.values()
