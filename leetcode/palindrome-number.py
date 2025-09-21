e968d1737a9edcd51392d427c51a49c29742e396
Supratik Bhowal <supratikbhowal23@gmail.com>
2025-09-21 14:11:59 +0530

    Update solution for palindrome-number

--- leetcode/palindrome-number.py
+++ leetcode/palindrome-number.py
@@ -1,30 +1,8 @@
-class Solution {
-public:
-    bool isPalindrome(int x) 
-    {
-        if (x >= 0)
-        {
-            int x2 = x;
-            double total = 0;
-            while (x2>0)
-            {
-                int digit = x2 % 10;
-                total = (total * 10) + (digit);
-                x2 = x2/10;
-            }
-
-            if (total == x)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        else
-        {
-            return false;
-        }
-    }
-};
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        a = str(x)
+        b = a[::-1]
+        if a==b:
+            return True
+        else:
+            return False
