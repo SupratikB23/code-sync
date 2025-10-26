db0e75b8d69a780a497f89e985892fe5d7aae78a
Supratik Bhowal <supratikbhowal23@gmail.com>
2025-10-26 22:02:25 +0530

    Update solution for change-data-type

--- leetcode/change-data-type.py
+++ leetcode/change-data-type.py
@@ -1,5 +1,4 @@
 import pandas as pd
 
 def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:
-    students["grade"] = students["grade"].convert_dtypes(convert_integer="true")
-    return(students)
+    return students.astype({'grade': int})
